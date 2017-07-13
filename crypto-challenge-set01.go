f2f5b45c7bb495c490b03798d4781340419a46ac
jdpleiness <jdpleiness@mars.dhe.oakland.edu>
2017-07-13 15:53:00 -0400

    set 1 challenges 1 and 2 complete

---
+++ crypto-challenge-set01.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+}
+
+func hexToBase64(src string) (out string) {
+	data, err := hex.DecodeString(src)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func xor(input string, key string) (out string) {
+	// function only works when input and key are same length
+	if len(input) != len(key) {
+		fmt.Println("Error, string lengths do not match")
+		return
+	}
+
+	rawinput, err := hex.DecodeString(input)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+
+	rawkey, err := hex.DecodeString(key)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+
+	rawslice := make([]byte, len(rawinput))
+
+	for i := 0; i < len(rawslice); i++ {
+		rawslice[i] = rawinput[i] ^ rawkey[i]
+	}
+
+	return hex.EncodeToString(rawslice)
+}
---
+++ crypto-challenge-set01_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestHextobase64(t *testing.T) {
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+	if hexToBase64(input) != expected {
+		t.Errorf("string returned was not expected")
+	}
+}
+
+func TestXor(t *testing.T) {
+	input := "1c0111001f010100061a024b53535009181c"
+	key := "686974207468652062756c6c277320657965"
+	expected := "746865206b696420646f6e277420706c6179"
+
+	if xor(input, key) != expected {
+		t.Errorf("string returned was not expected")
+	}
+}
