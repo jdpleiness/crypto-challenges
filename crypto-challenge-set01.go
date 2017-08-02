45657fba61c7ca56b2f00616dcc89bf43232c8e9
Jacob Pleiness <jdpleiness@oakland.edu>
2017-08-02 16:20:09 -0400

    set 1 challenges 1, 2, and 3 complete

--- crypto-challenge-set01.go
+++ crypto-challenge-set01.go
@@ -21,7 +21,7 @@ func hexToBase64(src string) (out string) {
 func xor(input string, key string) (out string) {
 	// function only works when input and key are same length
 	if len(input) != len(key) {
-		fmt.Println("Error, string lengths do not match")
+		fmt.Println("Error, string lengths do not match or key is not a single byte")
 		return
 	}
 
@@ -39,20 +39,63 @@ func xor(input string, key string) (out string) {
 
 	rawslice := make([]byte, len(rawinput))
 
-	for i := 0; i < len(rawslice); i++ {
+	for i := range rawslice {
 		rawslice[i] = rawinput[i] ^ rawkey[i]
 	}
 
 	return hex.EncodeToString(rawslice)
 }
 
-func singleByteXorDecode(input string) (outkey string, outmsg string) {
+func singleCharXor(input string, key byte) (out []byte) {
 	rawinput, err := hex.DecodeString(input)
 	if err != nil {
 		fmt.Println("error", err)
 		return
 	}
 
-	//TODO learn more about scoring character freq.
+	rawslice := make([]byte, len(rawinput))
 
+	for i := range rawslice {
+		rawslice[i] = rawinput[i] ^ key
+	}
+
+	return rawslice
+}
+
+func freqScore(input []byte) int {
+	/*
+		Score based on how many ascii chars it recognizes;
+		one point for normal, two for e/E, and three for space
+	*/
+	freqscore := 0
+
+	for _, char := range input {
+		if char > 31 && char < 127 {
+			freqscore++
+
+			if char == 32 {
+				freqscore += 2
+			} else if char == 69 || char == 101 {
+				freqscore++
+			}
+		}
+	}
+
+	return freqscore
+}
+
+func singleByteXorDecode(input string) (outkey string, outmsg string) {
+	highscore := 0
+	key := byte(0)
+
+	for char := byte(33); char <= 126; char++ {
+		score := freqScore(singleCharXor(input, char))
+
+		if score > highscore {
+			highscore = score
+			key = char
+		}
+	}
+
+	return string(key), string(singleCharXor(input, key))
 }
--- crypto-challenge-set01_test.go
+++ crypto-challenge-set01_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+)
 
-func TestHextobase64(t *testing.T) {
+func TestHexToBase64(t *testing.T) {
 	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
@@ -19,4 +23,39 @@ func TestXor(t *testing.T) {
 	if xor(input, key) != expected {
 		t.Errorf("string returned was not expected")
 	}
+
+}
+
+func TestSingleCharXor(t *testing.T) {
+	input := "1c0111001f010100061a024b53535009181c"
+	key := byte(33)
+	expected := "3d2030213e202021273b236a72727128393d"
+
+	if hex.EncodeToString(singleCharXor(input, key)) != (expected) {
+		fmt.Println(hex.EncodeToString(singleCharXor(input, key)))
+		t.Errorf("string returned was not expected")
+	}
+}
+
+func TestFreqScore(t *testing.T) {
+	input := "Hello World!"
+	expected := 15
+	if freqScore([]byte(input)) != expected {
+		t.Errorf("Did not return expected int value")
+	}
+}
+
+func TestSingleByteXorDecode(t *testing.T) {
+	input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	expectedmsg := "Cooking MC's like a pound of bacon"
+	expcetedkey := "X"
+
+	key, msg := singleByteXorDecode(input)
+
+	if msg != expectedmsg || key != expcetedkey {
+		fmt.Println("message: ", msg)
+		fmt.Println("key: ", key)
+		t.Errorf("key or msg did not match expected")
+	}
+
 }
